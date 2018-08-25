2a683ab4468c83e6a422ced31567ee066439d1e9
Juan Leon <jlp25@hotmail.com>
2018-08-24 23:07:03 -0500

    Add Bootstrap

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,4 +16,40 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('home/index.html.twig');
     }
+
+    /**
+     * @Route("/products", name="products")
+     */
+    public function productsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/new_product", name="new_product")
+     */
+    public function new_product_Action(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categoriesAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/new_category", name="new_category")
+     */
+    public function new_category_Action(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
 }
